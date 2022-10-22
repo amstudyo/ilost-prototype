17d981dbbbca453fd5e2b73db32a0f50f3ef6754
Amir Olphat <amirolphat@gmail.com>
2022-10-23 01:08:37 +0330

    All mechanics are scripted + main menu is added just for basic doing

---
+++ MainMenu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	
+	pass
+
+func _on_PlayVsAi_pressed():
+	get_tree().change_scene("res://BarzakhWoldScene.tscn")
+	pass
--- MainScript.gd
+++ MainScript.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 var damage_done = 0
+var damage_received = 0
+var kheir_shield = 0
+var shar_shield = 0
+var child1_bounded = 0
+var child2_bounded = 0
 
 func _ready():
 	var ViewPortSize = get_viewport().size
@@ -13,14 +18,20 @@ func _ready():
 	get_node("Ability_Hide").set_position(Vector2(50,ViewPortSizeY))
 	get_node("Ability_LightEffect").set_position(Vector2(200,ViewPortSizeY))
 	get_node("Ability_Thunder").set_position(Vector2(ViewPortSizeX+100,ViewPortSizeY))
+	get_node("Unbound_Child1").set_position(Vector2(ViewPortSizeX,ViewPortSizeY))
+	get_node("Unbound_Child2").set_position(Vector2(ViewPortSizeX-80,ViewPortSizeY))
 	get_node("Kheir").set_position(Vector2(300,get_viewport().size.y/2-20))
 	get_node("LightEffect").set_position(Vector2(300,get_viewport().size.y/2-20))
 	get_node("Shar").set_position(Vector2(get_viewport().size.x - 300,get_viewport().size.y/2-20))
+	get_node("DarkWood").set_position(Vector2(300,get_viewport().size.y/2+30))
+	get_node("Fire").set_position(Vector2(300,get_viewport().size.y/2+50))
+	#AI
+	get_node("Shar/Random_Ability_Timer").start()
 	pass
 
 #GAME OVER
 func _on_I_Lost_pressed():
-	get_tree().quit()
+	get_tree().change_scene("res://MainMenu.tscn")
 	pass
 
 #Kheir Ability: Hide 
@@ -29,27 +40,40 @@ func _on_Ability_Hide_pressed():
 	get_node("Ability_Hide/Ability_hide_usable_timer").start()
 	get_node("Kheir").visible = false
 	get_node("Ability_Hide").visible = false
+	kheir_shield = 1
 	pass
 func _on_Ability_Hide_timer_timeout():
 	get_node("Kheir").visible = true
+	kheir_shield = 0
 	pass
 func _on_Ability_hide_usable_timer_timeout():
 	get_node("Ability_Hide").visible = true
+	get_node("Ability_Hide/Ability_Hide_timer").stop()
+	get_node("Ability_Hide/Ability_hide_usable_timer").stop()
 	pass
 
-#Kheir Abilit: Thunder
+#Kheir Ability: Thunder
 func _on_Ability_Thunder_pressed():
 	get_node("Ability_Thunder/Ability_Thunder_Timer").start()
+	get_node("Ability_Thunder/Ability_Thunder_btn_Timer").start()
 	get_node("Thunder").visible = true
-	damage_done = damage_done + 1
-	get_node("Damage_Done").text = str(damage_done)
+	get_node("Ability_Thunder").visible = false
+	if shar_shield == 0:
+		Kheir_dmg()
 	pass
 func _on_Ability_Thunder_Timer_timeout():
 	get_node("Thunder").visible = false
 	pass
-
+func _on_Ability_Thunder_btn_Timer_timeout():
+	get_node("Ability_Thunder/Ability_Thunder_Timer").stop()
+	get_node("Ability_Thunder/Ability_Thunder_btn_Timer").stop()
+	get_node("Ability_Thunder").visible = true
+	pass
 #Kheir Ability: LightEffect
 func _on_Ability_LightEffect_pressed():
+	if shar_shield == 0:
+		Kheir_dmg()
+	get_node("Damage_Done").text = str(damage_done)
 	get_node("LightEffect").visible = true
 	get_node("Ability_LightEffect").visible = false
 	get_node("LightEffect/LightEffect_Timer").start()
@@ -67,4 +91,89 @@ func _on_LE_destroyer_timer_timeout():
 	get_node("Ability_LightEffect").visible = true
 	pass
 
+#Kheir Ability: Unbound Child1
+func _on_Unbound_Child1_pressed():
+	get_node("Unbound_Child1").visible = false
+	get_node("Kheir/Kheir_Child1").set_position(Vector2(93.507,187.013))
+	child1_bounded = 0
+	pass
+
+#Kheir Ability: Unbound Child2
+func _on_Unbound_Child2_pressed():
+	get_node("Unbound_Child2").visible = false
+	get_node("Kheir/Kheir_Child2").set_position(Vector2(243.117,187.013))
+	child2_bounded = 0
+	pass
+
+func Kheir_dmg():
+	if child1_bounded == 1 and child2_bounded == 0:
+		damage_done = damage_done + 2
+	elif child1_bounded == 0 and child2_bounded == 1:
+		damage_done = damage_done + 2
+	elif child1_bounded == 1 and child2_bounded == 1:
+		damage_done = damage_done + 1
+	else:
+		damage_done = damage_done + 3
+	get_node("Damage_Done").text = str(damage_done)
+	pass
+
 #Shar AI
+func _on_Random_Ability_Timer_timeout():
+	var randomize_attack_gen = RandomNumberGenerator.new()
+	randomize_attack_gen.randomize()
+	var randomize_attack = randomize_attack_gen.randf_range(0.0, 50.0)
+	if randomize_attack <= 10:
+		Shar_Fire()
+	elif randomize_attack <= 20:
+		Shar_DarkWood()
+	elif randomize_attack <= 30:
+		Shar_Invert()
+	elif randomize_attack <= 40:
+		Shar_Thief_Child1()
+	else:
+		Shar_Thief_Child2()
+	pass
+func Shar_Fire():
+	get_node("Fire").visible = true
+	get_node("Fire/Fire_Destroy_Timer").start()
+	if kheir_shield == 0:
+		damage_received = damage_received + 3
+		get_node("Damage_Recevied").text = str(damage_received)
+	pass
+func _on_Fire_Destroy_Timer_timeout():
+	get_node("Fire").visible = false
+	get_node("Fire/Fire_Destroy_Timer").stop()
+	pass
+func Shar_DarkWood():
+	get_node("DarkWood").visible = true
+	get_node("DarkWood/DarkWood_Destroy_Timer").start()
+	if kheir_shield == 0:
+		damage_received = damage_received + 3
+		get_node("Damage_Recevied").text = str(damage_received)
+	pass
+func _on_DarkWood_Destroy_Timer_timeout():
+	get_node("DarkWood").visible = false
+	get_node("DarkWood/DarkWood_Destroy_Timer").stop()
+	pass
+func Shar_Invert():
+	get_node("Shar").flip_v = true
+	get_node("Shar").set_position(Vector2(get_viewport().size.x - 300,get_viewport().size.y/5))
+	shar_shield = 1
+	get_node("Shar/Shar_Invert_Timer").start()
+	pass
+func _on_Shar_Invert_Timer_timeout():
+	get_node("Shar").flip_v = false
+	get_node("Shar").set_position(Vector2(get_viewport().size.x - 300,get_viewport().size.y/2-20))
+	shar_shield = 0
+	get_node("Shar/Shar_Invert_Timer").stop()
+	pass
+func Shar_Thief_Child1():
+	get_node("Kheir/Kheir_Child1").set_position(Vector2(get_viewport().size.x/2,get_viewport().size.y/2-20))
+	child1_bounded = 1
+	get_node("Unbound_Child1").visible = true
+	pass
+func Shar_Thief_Child2():
+	get_node("Kheir/Kheir_Child2").set_position(Vector2(get_viewport().size.x/2 + 150,get_viewport().size.y/2-20))
+	child2_bounded = 1
+	get_node("Unbound_Child2").visible = true
+	pass
